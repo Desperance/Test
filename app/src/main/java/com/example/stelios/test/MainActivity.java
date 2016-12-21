32f9b1d2f695bd5f6652b5703136f20ba0303634
Desperance <it1417@uom.edu.gr>
2016-12-21 15:02:21 +0200

    check here

--- app/src/main/java/com/example/stelios/test/MainActivity.java
+++ app/src/main/java/com/example/stelios/test/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // some changes here...
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
